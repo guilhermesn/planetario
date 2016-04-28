38149967b88a3d7731c93d6013553ff0364d8ba3
guilhermesn <guilherme.sn@live.com>
2016-04-28 17:35:08 -0300

    Merge origin/master
    Conflicts:
    	TrabalhoCOM220.git/src/CtrlPousada.java
    	TrabalhoCOM220.git/src/VisaoPousada.java

---
+++ Planetario/src/planetario/ConectaBD.java
@@ -0,0 +1,17 @@
+
+package planetario;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Iterator;
+import java.util.Scanner;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class ConectaBD {
+    
+    
+    
+    
+    
+}
